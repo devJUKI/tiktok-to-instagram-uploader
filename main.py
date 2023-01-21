11f51c7e8293644d9299ce24e1520893c2250e44
devJUKI <49684752+devJUKI@users.noreply.github.com>
2023-01-21 04:48:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from instagram_uploader import InstagramUploader
 from tiktok_watermark_remover import TikTokWatermarkRemover
+from facebook_api import FacebookAPI
 
-instagram_account_id = 'XXXXXXXXXX'
-access_token = 'XXXXXXXXXX'
-caption = "XXXXXXXXXX"
+instagram_account_id = '17841456683250303'
+caption = "HOW GOOD DOES THIS LOOK? 🔥 LINK IN BIO 🎁 #musthaves #flame #diffuser #fire #gift #giftideas #decoration #air #humidifier #Relaxation #ComfortLiving #AirQuality #HomeImprovement #HealthyLiving"
 share_to_feed = "true"
 thumb_offset = 0
 
+facebook_api = FacebookAPI()
+facebook_access_token = facebook_api.get_access_token()
+
 # Get TikTok video url without watermark
 print('Getting TikTok video without watermark...')
-tiktok_url = 'XXXXXXXXXX'
+tiktok_url = 'https://www.tiktok.com/@higrowth/video/7127924895127850246?q=flame%20humidifier&t=1674269115892'
 watermark_remover = TikTokWatermarkRemover()
 video_url = watermark_remover.get_video_url(tiktok_url)
 
 # Upload video to Instagram
 print('Starting to upload reel to Instagram...')
-ig_uploader = InstagramUploader(instagram_account_id, access_token)
+ig_uploader = InstagramUploader(instagram_account_id, facebook_access_token)
 ig_uploader.upload_reel(caption, share_to_feed, thumb_offset, video_url)
 
 print('Done')
